3251c521359298552517b1d3bd3f1d7e998422dc
khoaaho <khoaho@utexas.edu>
2021-11-09 12:31:10 -0600

    Added Registration View Controller
    added screen for registering new users. Enabled user auth in the firebase console.

---
+++ Volly/RegistrationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RegistrationViewController.swift
+//  Volly
+//
+//  Created by Khoa Ho on 11/9/21.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
