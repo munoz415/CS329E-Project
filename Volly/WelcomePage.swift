fb0f89587ca819ea8efba9cfccf62c5ab062b38f
kyhoover107@gmail.com <kyhoover107@gmail.com>
2021-11-16 17:25:53 -0600

    Welcome Page

--- Volly/WelcomePage.swift
+++ Volly/WelcomePage.swift
@@ -15,6 +15,28 @@ class WelcomePage: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "addEvents",
+           let nextVC = segue.destination as? AddEventViewController{
+            //delegate self
+            //nextVC.delegate = self
+           
+        }
+        if segue.identifier == "Calendar",
+           let nextVC = segue.destination as? CalendarViewController{
+            //delegate self
+            //nextVC.delegate = self
+           
+        }
+        if segue.identifier == "pastEvents",
+           let nextVC = segue.destination as? PastEventsViewController{
+            //delegate self
+            //nextVC.delegate = self
+           
+        }
+    }
 
 }
 
