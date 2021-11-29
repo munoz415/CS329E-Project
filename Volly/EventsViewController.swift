839d1519f5d4a7476291fc8fd7b5bb1bf78043b1
dillant <92829671+dillant@users.noreply.github.com>
2021-11-29 15:22:18 -0600

    Update EventsViewController.swift
    So what's been done is that a tableview is made, but cells only display one line. Also can't figure out how to retrieve user specific data (i had one account with events, another without, and the empty user came up with the same results as the one with events). Added swipe to delete func that swipes but doesn't delete.

--- Volly/EventsViewController.swift
+++ Volly/EventsViewController.swift
@@ -5,10 +5,18 @@
 import UIKit
 import CoreData
 
-class EventsViewController: UIViewController, UNUserNotificationCenterDelegate {
+class EventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UNUserNotificationCenterDelegate {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    var eventList:[NSManagedObject] = []
+    @IBOutlet weak var tableView: UITableView!
+    let textCellIdentifier = "TextCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         UNUserNotificationCenter.current().delegate = self
         
         let fetchedResults = retrieveEvent()
@@ -19,18 +27,25 @@ class EventsViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
 
         // Do any additional setup after loading the view.
+        print(eventList)
+//        print(fetchedResults)
     }
     
     func retrieveEvent() -> [NSManagedObject] {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        let context = appDelegate.persistentContainer.viewContext
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName:"EventEntity")
         var fetchedResults:[NSManagedObject]? = nil
         
         do {
             try fetchedResults = context.fetch(request) as? [NSManagedObject]
-        } catch {
+            self.eventList = fetchedResults!
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch {
             // If an error occurs
             let nserror = error as NSError
             NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
@@ -39,6 +54,53 @@ class EventsViewController: UIViewController, UNUserNotificationCenterDelegate {
         
         return(fetchedResults)!
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
+        let eventRow = eventList[indexPath.row]
+        cell.textLabel?.text =  "\(eventRow.value(forKey: "name")) \n" +
+                                "\(eventRow.value(forKey: "date")) \n" +
+                                "\(eventRow.value(forKey: "eventDescription")) \n" +
+                                "\(eventRow.value(forKey: "hours")) \n" +
+                                "\(eventRow.value(forKey: "location")) "
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventList.count
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let action = UIContextualAction(style: .destructive, title: "Delete") {(action, view, completionhandler) in
+            
+            // Which event to remove
+            let eventToRemove = self.eventList[indexPath.row]
+            
+            // remove event
+            self.context.delete(eventToRemove)
+            
+            // save the data
+            do {
+                try self.context.save()
+            }
+            catch {
+            }
+            
+            self.retrieveEvent()
+            }
+        
+        return UISwipeActionsConfiguration(actions: [action])
+        }
+    
+    
+    
+    
+    
     
 
     /*
