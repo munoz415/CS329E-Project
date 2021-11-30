de02c7f7dbb73e8ec3b74e990170a6bd53329066
kyhoover107@gmail.com <kyhoover107@gmail.com>
2021-11-29 21:29:12 -0600

    Added Profile Picture and completed settings page layout

--- Volly/SettingsViewController.swift
+++ Volly/SettingsViewController.swift
@@ -5,13 +5,32 @@
 import UIKit
 import CoreData
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var nameField: UITextField!
-    
+    @IBOutlet weak var imageView: UIImageView!
+    let picker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        picker.delegate = self
         // Do any additional setup after loading the view.
+    }
+    @IBAction func pictureButtonpressed(_ sender: Any) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // "info" contains a dictionary of information about the selected media, including:
+        // - metadata
+        // - a user-edited image (if we set the allowsEditing property to true)
+        
+        let chosenImage = info[.originalImage] as! UIImage
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = chosenImage
+        
+        dismiss(animated: true, completion: nil)
+        
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
@@ -20,6 +39,12 @@ class SettingsViewController: UIViewController {
             storeSettings()
     }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true, completion: nil)
+        }
+        
+        
+        
         func storeSettings() {
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
             let context = appDelegate.persistentContainer.viewContext
